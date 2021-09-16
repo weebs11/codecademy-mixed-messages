1e1deefae1ff691b30e123686a9a46ef802ce262
weebs11 <kwehbi@gmail.com>
2021-09-15 22:15:25 -0500

    adjust chooseWord function and add comments

--- messages.js
+++ messages.js
@@ -34,7 +34,19 @@ const adjective = [
   'ugly',
   'wild',
 ];
-const verbPast = [];
+const adverb = [
+  'abnormally',
+  'enthusiastically',
+  'hourly',
+  'joyfully',
+  'limply',
+  'nervously',
+  'patiently',
+  'rigidly',
+  'sharply',
+  'urgently',
+  'wildly',
+];
 const name = [
   'Kelly',
   'Elizabeth',
@@ -45,12 +57,46 @@ const name = [
   'Warren',
   'Bailey',
 ];
-const bodyPart = [];
-const pluralNoun = [];
+const bodyPart = [
+  'hand',
+  'face',
+  'leg',
+  'nose',
+  'arm',
+  'butt',
+  'finger',
+  'chest',
+  'toe',
+];
+const pluralNoun = [
+  'women',
+  'men',
+  'balloons',
+  'pickles',
+  'speakers',
+  'baseballs',
+  'babies',
+  'screwdrivers',
+];
+const room = [
+  'living room',
+  'kitchen',
+  'dining room',
+  'bedroom',
+  'bathroom',
+  'garage',
+];
 
-const chooseWord = (partOfSpeech) =>
-  partOfSpeech[Math.floor(Math.random() * partOfSpeech.length)];
+// Function to randomly selected a word from the array of the supplied part of speech.
+// The function will remove the word from the array in order to avoid duplicate selections in the Mad Lib.
+const chooseWord = (partOfSpeech) => {
+  let x = Math.floor(Math.random() * partOfSpeech.length);
+  let word = partOfSpeech[x];
+  partOfSpeech.splice(x, 1); // Unsure if there's a way to splice the array without storing the index in a new variable (word).
+  return word;
+};
 
+// Function to print the Mad Lib to the console. The function accepts a random number (0-3) to choose the Mad Lib., and calls the chooseWord function for each choice.
 const buildString = (madLib) => {
   switch (madLib) {
     case 0:
@@ -61,7 +107,25 @@ const buildString = (madLib) => {
       );
       break;
     case 1:
-      console.log('test2');
+      console.log(
+        `Ye can always pretend to be a bloodthirsty ${chooseWord(
+          noun
+        )}, threatening everyone by waving her ${chooseWord(
+          adjective
+        )} sword in the air, but until ye learn to ${chooseWord(
+          verb
+        )} like a pirate, ye'll never be ${chooseWord(
+          adverb
+        )} acepted as an authentic ${chooseWord(
+          noun
+        )}. So here's what ye do: Cleverly work into yer daily conversations ${chooseWord(
+          adjective
+        )} pirate phrases such as "Ahoy there, ${chooseWord(
+          pluralNoun
+        )}," "Avase, ye ${chooseWord(pluralNoun)}," and "Shiver me ${chooseWord(
+          pluralNoun
+        )}."`
+      );
       break;
     case 2:
       console.log('test3');
